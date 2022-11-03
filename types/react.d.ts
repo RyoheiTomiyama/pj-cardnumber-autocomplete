c691e773b98b71040a53739ecebe02d19b2cc77b
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2022-11-03 22:23:43 +0900

    fix types

--- types/react.d.ts
+++ types/react.d.ts
@@ -1,6 +1,4 @@
 // React18からchildrenを明示しないといけなくなったので、children持ち用の型を定義する
-import { FC, ReactNode } from 'react'
-
 declare namespace React {
   type FCX<P = Record<string, unknown>> = FC<P & { children?: ReactNode }>
 }
