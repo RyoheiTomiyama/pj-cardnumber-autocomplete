f3fcbf4259eea8de80949d50c2aa4f462dcb75aa
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2025-03-24 18:12:47 +0900

    fix: PropsWithChildrenで不要になった拡張型を削除

--- components/layout/Section.tsx
+++ components/layout/Section.tsx
@@ -1,8 +1,8 @@
-import type React from 'react'
+import type { PropsWithChildren } from 'react'
 
 import styles from './Section.module.css'
 
-const Section: React.FCX = ({ children }) => {
+const Section: React.FC<PropsWithChildren> = ({ children }) => {
   return <div className={styles.wrapper}>{children}</div>
 }
 
--- types/react.d.ts
+++
@@ -1,4 +0,0 @@
-// React18からchildrenを明示しないといけなくなったので、children持ち用の型を定義する
-declare namespace React {
-  type FCX<P = Record<string, unknown>> = FC<PropsWithChildren<P>>
-}
