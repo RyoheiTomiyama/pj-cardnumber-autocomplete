d843a13d8de6768cc8881f8a2e1398116913b13a
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2025-03-24 19:03:14 +0900

    fix: importFromがdeprecatedされていたので修正
    vercelでビルド落ちた

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
-import globals from 'globals'
 import { FlatCompat } from '@eslint/eslintrc'
 import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import pluginReact from 'eslint-plugin-react'
 import pluginReactJSXRuntime from 'eslint-plugin-react/configs/jsx-runtime.js'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
-import eslintConfigPrettier from 'eslint-config-prettier'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
@@ -13,11 +13,25 @@ const compat = new FlatCompat({
 })
 
 /** @type {import('eslint').Linter.Config[]} */
+const tsConfig = [
+  ...tseslint
+    .config(tseslint.configs.recommended, {
+      rules: {
+        '@typescript-eslint/consistent-type-imports': 'error',
+      },
+    })
+    .map((c) => ({
+      ...c,
+      files: ['**/*.{ts,tsx}'],
+    })),
+]
+
+/** @type {import('eslint').Linter.Config[]} */
 const config = [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tsConfig,
   pluginReact.configs.flat.recommended,
   pluginReactJSXRuntime,
   {
@@ -33,11 +47,6 @@ const config = [
     extends: ['next/core-web-vitals'],
   }),
   eslintConfigPrettier,
-  {
-    rules: {
-      '@typescript-eslint/consistent-type-imports': 'error',
-    },
-  },
 ]
 
 export default config
